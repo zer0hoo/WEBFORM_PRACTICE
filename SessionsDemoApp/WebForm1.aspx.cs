3b22ca588d3f13c5806418bb1492c5359c9baeac
김영후 <kimyhoo@krs.co.kr>
2025-04-14 14:03:42 +0900

    session

---
+++ SessionsDemoApp/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionsDemoApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int n = 0;
+            if (Session.IsNewSession)
+                n = (int) Session["cnt"];
+            n++;
+            Session["cnt"] = n;
+            lblCounter.Text = n.ToString();
+        }
+    }
+}
